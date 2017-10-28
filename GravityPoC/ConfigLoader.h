f0f5a1c04481cd4144356af385d234baed5e8371
Daniel Ferguson <daniel.j.ferguson@intel.com>
2017-10-27 23:01:46 -0700

    Add much iron to fire. Let eggs cool.
    * Collision Response is very stable.
    * Existing issues can almost be explained.
    * Added Friction
    * Added configuration file for physics parameters.
    * Added ConfigLoader to read config parameters from file

---
+++ GravityPoC/ConfigLoader.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+
+namespace bali
+{
+        struct PhysicsConfig
+        {
+            int   HORIZONTAL_MOVE_ANGLE_OFFSET;// = 0;
+            int   MOVE_STRENGTH;// = 50;
+            int   GRAVITY_CONSTANT;// = 1000;
+            float FIXED_DELTA;// = 1.0f / 20000.0f;
+            float DRAG_CONSTANT;// = 0.00009;
+            float RESTITUTION;// = 0.30f;
+            float FAST_THRESHOLD;// = 30.0f;
+            float KINETIC_FRICTION;// = 1.75f;
+            float DYNAMIC_FRICTION;// = 0.10f;
+            float FAST_JUMP_THRESHOLD;
+            float JUMP_STRENGTH;
+            float MAX_VELOCITY;
+        };
+
+        PhysicsConfig loadPhysicsConfig(std::string filename);
+
+}
+
+
